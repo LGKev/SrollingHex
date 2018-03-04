5465e07d97d7ef587a352ac02a1ffcf4cc4f0320
Kevin Kuwata <kevinkuwata@icloud.com>
2018-03-04 14:10:00 -0700

    I have a barrel shift working == roll, but its only good for 32 bits.... 4 characters. unless I get creative

--- simple_program.s
+++ simple_program.s
@@ -10,7 +10,8 @@ _start:
 	movia	r3, SWITCHES	# Address of switches
 	movia 	r5, HEX0		# move r5 to be the hex values
 	movia 	r8,	HEX1
-	movia 	r6, 	0b00000000	#all off
+	#movia 	r6, 	0b00000000	#all off
+	movia 	r6,		0b011101100011110100000000
 	movia 	r7, 	0b11111111	#LIT
 	movia 	r9, 	0b01110110	#H
 	movia 	r10, 	0b00111101 #E
@@ -18,16 +19,17 @@ _start:
 
 	movia 	r11, 5			#execute 4 times, for the 4 display	
 	movia 	r13, 9000000		#delay of 9 million cycles?
+	movia 	r14,8
 
 LOOP:
 
 #	add 	r6, r6, r9		#add r9 (H) to the r6 register.
 
-	mov		r6, r9			#copy r9 into r6
+	#mov		r6, r9			#copy r9 into r6
 	#erase previous display to get scroll. done automatically by the shift left logical imiidiate (SLLI), fills with 0
 	stwio	r6, 0(r5)		#display newest thing to screen, display r6 contents to hex
-	slli	r9, r9, 8		#shift the H now
-
+	#slli	r9, r9, 8		#shift the H now
+	rol		r6, r6, r14		# roll r6 left by r14 (which is 8). roll it 8 rotate, barrel shift
 		
 
 	subi 	r11, r11, 1		#DECREMENT, should go 4 times	
