df129e015f1835bac2c6e913391a9ee58154e5f2
Kevin Kuwata <kevinkuwata@icloud.com>
2018-03-04 13:23:04 -0700

    able to step through assembly to have an H scroll accross 4 hex displays, need to do timing scheme to make h slowly transition

--- simple_program.s
+++ simple_program.s
@@ -1,14 +1,51 @@
 	.text
 	.equ	LEDs,		0xFF200000
 	.equ	SWITCHES,	0xFF200040
+	.equ 	HEX0,		0xFF200020
+	.equ	HEX1,		0xFF200028
+	.equ	x,			0
 	.global _start
 _start:
 	movia	r2, LEDs		# Address of LEDs         
 	movia	r3, SWITCHES	# Address of switches
+	movia 	r5, HEX0		# move r5 to be the hex values
+	movia 	r8,	HEX1
+	movia 	r6, 	0b00000000	#all off
+	movia 	r7, 	0b11111111	#LIT
+	movia 	r9, 	0b01110110	#H
+	movia 	r10, 	0b00111101 #E
+	stwio	r0, 0(r5)		# Display value off to HEX0
+
+	movia 	r11, x		
 
 LOOP:
-	ldwio	r4, (r3)		# Read the state of switches
-	stwio	r4, (r2)		# Display the state on LEDs
+
+#	add 	r6, r6, r9		#add r9 (H) to the r6 register.
+
+	mov		r6, r9			#copy r9 into r6
+	#erase previous display to get scroll. done automatically by the shift left logical imiidiate (SLLI), fills with 0
+	stwio	r6, 0(r5)		#display newest thing to screen, display r6 contents to hex
+	slli	r9, r9, 8		#shift the H now
+
+		
+
+	subi 	r11, r11, 1		#DECREMENT, should go 4 times	
+#will probably be looping very fast so everything will look lit up.
+#at least 4 of them
+
+	#stwio 	r7, 4(r5)
+	beq		r11, r0, END
 	br		LOOP
 
+
+
+END:
+	br END
+
+
 	.end
+
+
+
+.data
+x:	.word	4	#counter so we know to loop 4 times
