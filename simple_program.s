606714ffa714c2609fb695bcb5a5804f895bb406
Kevin Kuwata <kevinkuwata@icloud.com>
2018-03-10 13:32:57 -0700

    I was albe to use an array for the hello_buffs---___, now I am going to add flow control sO I can do infinite loop of the message scrolll, as well as the alternating patterns. alternating patterns will be created with Array_A, array_B, and array_C, an dporabably a for loop

--- simple_program.s
+++ simple_program.s
@@ -1,32 +1,31 @@
 	.text
-	.equ	LEDs,		0xFF200000
-	.equ	SWITCHES,	0xFF200040
-	.equ 	HEX0,		0xFF200020
-	.equ	HEX1,		0xFF200028
-	.equ	x,			0
+	.equ 	HEX0,		0xFF200020	#hex0 address base
+	.equ	HEX_INDEX,			4	# loop over the 4 hexes
+	.equ	WAIT_DELAY,			9000000 #I think this might be redundent because wait_dealy is decalred in data.
+	.equ	LetterArraySize,		17	#currently we only have []hello_ _ _ in the array
 	.global _start
 _start:
-	movia	r2, LEDs		# Address of LEDs         
-	movia	r3, SWITCHES	# Address of switches
-	movia 	r5, HEX0		# move r5 to be the hex values
-	movia 	r8,	HEX1
-	movia 	r6, 	0b00000000	#all off
-	movia 	r7, 	0b11111111	#LIT
-	movia 	r9, 	0b01110110	#H
-	movia 	r10, 	0b00111101 #E
+	movia 	r5, HEX0		# move r5 to be the hex base address.
+#make an index
+	movia 	r6, LetterArray	#points to first element. offsets allow next element. in array
+	ldw		r9, 0(r6)		#load the first element into r9
 	stwio	r0, 0(r5)		# Display value off to HEX0
-
-	movia 	r11, 5			#execute 4 times, for the 4 display	
+	movia 	r11, LetterArraySize			#execute 4 times, for the 4 display	
 	movia 	r13, 9000000		#delay of 9 million cycles?
+	 	
 
 LOOP:
 
 #	add 	r6, r6, r9		#add r9 (H) to the r6 register.
 
-	mov		r6, r9			#copy r9 into r6
+	#mov		r6, r9			#copy r9 into r6 #REDUNDENT and poor use of memory /registers. delete
 	#erase previous display to get scroll. done automatically by the shift left logical imiidiate (SLLI), fills with 0
-	stwio	r6, 0(r5)		#display newest thing to screen, display r6 contents to hex
+	stwio	r9, 0(r5)		#display newest thing to screen, display r6 contents to hex
 	slli	r9, r9, 8		#shift the H now
+	#get next letter: increment array index, add to r9
+	addi 	r6, r6, 4		#increment the index/ increment the address.
+	ldw		r7, 0(r6)		#load value at that address at r6, load into r7, for keeping, add to r9
+	add		r9, r7, r9
 
 		
 
@@ -44,8 +43,8 @@ LOOP:
 
 END:
 #RESET the h 
-	movia r9, 0b01110110
-	movia r11, 5
+	movia r6, LetterArray
+	movia r11, LetterArraySize
 	br LOOP
 
 Delay:
@@ -53,9 +52,14 @@ Delay:
 	beq r12, r13, LOOP
 	br Delay
 	
-.end
 
 
 
 .data
-x:	.word	4	#counter so we know to loop 4 times
+#HEX_INDEX:	.word	4	#counter so we know to loop 4 times
+#WAIT_DELAY: .word 	9000000	# simple counter based delay. 
+LetterArray: 
+	.word  0x00, 0x76, 0x79, 0x38, 0x38,0x3F, 0x00, 0x7F, 0x3E, 0x71, 0x71, 0x6D, 0x40, 0x40,0x40, 0x00, 0x00, 0x00 
+		# off, H	, E ,	L,		L,   0,		_, B, U, F, F, S, _, _
+
+.end #put this at the end!
